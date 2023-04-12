a7a20e5f4f70d02403de3b5b44898a39f4820e40
diegogh1234 <diegogh_1234@hotmail.com>
2023-04-11 21:19:55 -0500

    avance

---
+++ EXERCISE/app/src/main/java/com/example/exercise/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.exercise
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESS= "com.example.app_sem04_s1_1"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.etName)
+        val message=editText.text.toString()
+
+        val intent= Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESS,message)
+        }
+        startActivity(intent)
+    }
+}
